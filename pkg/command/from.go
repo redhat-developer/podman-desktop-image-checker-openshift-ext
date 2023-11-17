b076ad153d047d1169d54f5c5f773b86ead0ebd7
Jeff MAURY <jmaury@redhat.com>
2023-11-17 15:22:29 +0100

    fix: apply comments from @feloy
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- pkg/command/from.go
+++ pkg/command/from.go
@@ -23,21 +23,22 @@ type From struct {
 const SCRATCH_IMAGE_NAME = "scratch"
 
 func (f From) Analyze(node *parser.Node, source utils.Source, line Line) []error {
+	if node.Value == SCRATCH_IMAGE_NAME {
+		return nil
+	}
 	errs := []error{}
-	if node.Value != SCRATCH_IMAGE_NAME {
-		decompiledNode, err := decompiler.Decompile(node.Value)
-		if err != nil {
-			// unable to decompile base image
-			errs = append(errs, fmt.Errorf("unable to analyze the base image %s", node.Value))
-			return errs
-		}
-		errsFromBaseImage := AnalyzeNodeFromSource(decompiledNode, utils.Source{
-			Name: node.Value,
-			Type: utils.Parent,
-		})
-		if len(errsFromBaseImage) > 0 {
-			errs = append(errs, errsFromBaseImage...)
-		}
+	decompiledNode, err := decompiler.Decompile(node.Value)
+	if err != nil {
+		// unable to decompile base image
+		errs = append(errs, fmt.Errorf("unable to analyze the base image %s", node.Value))
+		return errs
+	}
+	errsFromBaseImage := AnalyzeNodeFromSource(decompiledNode, utils.Source{
+		Name: node.Value,
+		Type: utils.Parent,
+	})
+	if len(errsFromBaseImage) > 0 {
+		errs = append(errs, errsFromBaseImage...)
 	}
 	return errs
 }
